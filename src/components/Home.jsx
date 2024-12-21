d2c6385f3fdeeb38ecc2fa8e2c2da72c1b084b2e
Arsla-1 <105283537+Arsla-1@users.noreply.github.com>
2024-12-21 18:50:14 +0530

    fixed portfolio button

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -20,12 +20,12 @@ const Home = () => {
             with expertise in MERN stack and major CMS like Wordpress, Shopify
             and Webflow.
           </p>
-          <div>
+          <div className="flex">
             <Link
               to="portfolio"
               smooth
               duration={500}
-              className="group text-white px-6 py-3 my-2 flex items-center rounded-md bg-gradient-to-r from-cyan-500 to-blue-500 cursor-pointer justify-self-start"
+              className="group text-white px-6 py-3 my-2 flex items-center rounded-md bg-gradient-to-r from-cyan-500 to-blue-500 cursor-pointer"
             >
               Portfolio
               <span className="group-hover:rotate-90 duration-300">
