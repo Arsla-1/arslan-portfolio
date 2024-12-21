e79e0971de5a274421468608cf7f94b098db48b7
Arsla-1 <105283537+Arsla-1@users.noreply.github.com>
2024-12-21 12:54:13 +0530

    added resume

--- src/components/SocialLinks.jsx
+++ src/components/SocialLinks.jsx
@@ -40,7 +40,7 @@ const SocialLinks = () => {
           Resume <BsFillPersonLinesFill size={30} />
         </>
       ),
-      href: "/Resume-Arslan Shahid.pdf",
+      href: "/Arslan_resume.pdf",
       style: "rounded-br-md",
       download: true,
     },
